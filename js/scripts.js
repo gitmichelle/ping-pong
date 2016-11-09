6fc0fe48ede8ac87906772f3bc88daf13a9dd030
Michelle Llaguno <michellellaguno@gmail.com>
2016-11-08 21:01:57 -0800

    add the basic html, including rules, basic css, and basic js functionality

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function() {
+  $("form#pingpongnumber").submit(function(event) {
+    event.preventDefault();
+
+
+    var inputtedNumber = parseInt($("input#numberinput").val());
+
+
+    for (i = inputtedNumber; i > 0; i--) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        $("ul#resultlist").append("<li>" + "pingpong" + "</li>");
+      } else if (i % 5 === 0) {
+        $("ul#resultlist").append("<li>" + "pong" + "</li>");
+      } else if (i % 3 === 0) {
+        $("ul#resultlist").append("<li>" + "ping" + "</li>");
+      } else {
+        $("ul#resultlist").append("<li>" + i + "</li>");
+      };
+    };
+  });
+});
